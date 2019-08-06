fa60ce3675ae4524376a4d70597a30dd8cca675e
n-seiji <49780855+n-seiji@users.noreply.github.com>
2019-08-06 23:18:37 +0900

    UIの改善

---
+++ 駐ring/Custom_Button.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+@IBDesignable
+class Custom_Button: UIButton {
+    
+    @IBInspectable var textColor: UIColor?
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+}
